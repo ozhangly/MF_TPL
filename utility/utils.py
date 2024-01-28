89cd22162d69fe3c611e4582c5f849203ae8e836
Ross <KAlvYY@163.com>
2024-01-28 20:21:25 +0800

    utils

---
+++ utility/utils.py
@@ -0,0 +1,10 @@
+import os
+
+
+def ensure_dir(dir_path: str) -> None:
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+
+def file_exists(file_path: str) -> bool:
+    return os.path.exists(file_path)
