672b1db4465849cdd2d533b855e63fd242702a88
Ross <KAlvYY@163.com>
2024-01-28 22:09:20 +0800

    add args

--- utility/args_parser.py
+++ utility/args_parser.py
@@ -11,23 +11,23 @@ def parse_args():
 
     parser.add_argument('--epochs', nargs='?', default=10, type=int)
 
-    parser.add_argument('--top k', nargs='?', default=6, type=int)
+    parser.add_argument('--top_k', nargs='?', default=6, type=int)
 
     parser.add_argument('--alpha', nargs='?', default=0.5, type=float)
 
     # 实验的路径
-    parser.add_argument('--relation path', nargs='?', default='./relation/', type=str)
+    parser.add_argument('--relation_path', nargs='?', default='./relation/', type=str)
 
-    parser.add_argument('--similarity path', nargs='?', default='./similarity/', type=str)
+    parser.add_argument('--similarity_path', nargs='?', default='./similarity/', type=str)
 
-    parser.add_argument('--rec output', nargs='?', default='./output/', type=str)
+    parser.add_argument('--rec_output', nargs='?', default='./output/', type=str)
 
-    parser.add_argument('--training path', nargs='?', default='./training dataset/', type=str)
+    parser.add_argument('--training_path', nargs='?', default='./training dataset/', type=str)
 
-    parser.add_argument('--testing path', nargs='?', default='./testing dataset/', type=str)
+    parser.add_argument('--testing_path', nargs='?', default='./testing dataset/', type=str)
 
-    parser.add_argument('--training dataset', nargs='?', default='train_GREC_0_1.json', type=str)
+    parser.add_argument('--training_dataset', nargs='?', default='train_GREC_0_1.json', type=str)
 
-    parser.add_argument('--testing dataset', nargs='?', default='testing_0_removed_num_1.json', type=str)
+    parser.add_argument('--testing_dataset', nargs='?', default='testing_0_removed_num_1.json', type=str)
 
     return parser.parse_args()
