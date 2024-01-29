6134c7257be9d7664234d30c6231d5805232a279
Ross <KAlvYY@163.com>
2024-01-29 09:47:44 +0800

    sub memory occupy

--- utility/load_data.py
+++ utility/load_data.py
@@ -11,7 +11,7 @@ def load_relation_mat(train_file_path: str) -> np.ndarray:
     rmv_fold = re.findall('[0-9]', args.training_dataset)
     relation_file_name = 'relation_%s_%s.txt' % (rmv_fold[0], rmv_fold[1])
     if os.path.exists(args.relation_path + relation_file_name):
-        relation = np.loadtxt(args.relation + relation_file_name)
+        relation = np.loadtxt(args.relation + relation_file_name, dtype=np.int8)
         return relation
 
     size_apk: int = 0
@@ -24,8 +24,9 @@ def load_relation_mat(train_file_path: str) -> np.ndarray:
         size_apk = max(size_apk, app_id)
         size_lib = max(max(tpl_list), size_lib)
 
-    relation = np.zeros(shape=(size_apk, size_lib))
+    relation = np.zeros(shape=(size_apk, size_lib), dtype=np.int8)
 
+    train_fp.seek(0, 0)
     for line in train_fp.readlines():
         obj = json.loads(line.strip('\n'))
         app_id = obj['app_id']
@@ -33,6 +34,6 @@ def load_relation_mat(train_file_path: str) -> np.ndarray:
         for tpl in tpl_list:
             relation[app_id, tpl] = 1
 
-    np.savetxt(fname=args.relation_path + relation_file_name, X=relation)
+    np.savetxt(fname=args.relation_path + relation_file_name, X=relation, fmt='%d')
 
     return relation
