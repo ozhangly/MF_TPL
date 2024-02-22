0558607152bae29a4c77da9988399fe0ca427291
MaxBlack <963137330@qq.com>
2024-02-22 11:11:34 +0800

    add dict process function

--- utility/utils.py
+++ utility/utils.py
@@ -1,5 +1,6 @@
 import os
 
+from typing import Dict
 
 def ensure_dir(dir_path: str) -> None:
     if not os.path.exists(dir_path):
@@ -8,3 +9,10 @@ def ensure_dir(dir_path: str) -> None:
 
 def file_exists(file_path: str) -> bool:
     return os.path.exists(file_path)
+
+
+def process_dict_key(src_dict: Dict) -> Dict:
+    tgt_dict = {}
+    for key, val in src_dict.items():
+        tgt_dict[int(key)] = val
+    return tgt_dict
