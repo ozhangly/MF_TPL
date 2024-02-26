3094facae16372bed20de1edced65fa2d96f3193
MaxBlack <963137330@qq.com>
2024-02-26 17:07:59 +0800

    the args

--- utility/args_parser.py
+++ utility/args_parser.py
@@ -5,7 +5,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weight', nargs='?', default=24, type=int)
 
-    parser.add_argument('--lmda', nargs='?', default=0.001, type=float)
+    parser.add_argument('--lmda', nargs='?', default=0.01, type=float)
 
     parser.add_argument('--factor', nargs='?', default=20, type=int)
 
@@ -26,7 +26,7 @@ def parse_args():
 
     parser.add_argument('--testing_path', nargs='?', default='./testing dataset/', type=str)
 
-    parser.add_argument('--training_dataset', nargs='?', default='train_GREC_0_1.json', type=str)
+    parser.add_argument('--training_dataset', nargs='?', default='train_MF_0_1.json', type=str)
 
     parser.add_argument('--testing_dataset', nargs='?', default='testing_0_removed_num_1.json', type=str)
 
