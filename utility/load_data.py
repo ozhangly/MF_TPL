0bd178404355e2462b6e96aa23a302178b9a65b3
MaxBlack <963137330@qq.com>
2024-02-27 11:57:47 +0800

    optimize the way

--- utility/load_data.py
+++ utility/load_data.py
@@ -10,38 +10,43 @@ from typing import Dict, Tuple
 args = utility.config.args
 
 
-def load_relation_mat(train_file_path: str) -> Tuple[np.ndarray, Dict, Dict]:
+def load_relation_mat(train_file_path: str) -> np.ndarray:
     fold_rmv = re.findall('[0-9]', args.training_dataset)
     relation_file_name = 'relation_%s_%s.txt' % (fold_rmv[0], fold_rmv[1])
-    # 还有两个字典需要保存
-    dict1_name = 'app_order_id2app_id_%s_%s.json' % (fold_rmv[0], fold_rmv[1])
-    dict2_name = 'app_id2app_order_id_%s_%s.json' % (fold_rmv[0], fold_rmv[1])
+    # app的映射哈希表
+    dict1_name = 'app_id2app_order_id.json'
+
+    # lib的映射哈希表
+    lib_dict1_name: str = 'lib_order_id2lib_id.json'
+
     if os.path.exists(args.relation_path + f'fold{fold_rmv[0]}_rmv{fold_rmv[1]}/' + relation_file_name):
         relation = np.loadtxt(args.relation_path + f'fold{fold_rmv[0]}_rmv{fold_rmv[1]}/' + relation_file_name, dtype=np.int8)
-        app_order_id2app_id = json.load(fp=open(file=args.relation_path + f'fold{fold_rmv[0]}_rmv{fold_rmv[1]}/' + dict1_name, mode='r'))
-        app_id2app_order_id = json.load(fp=open(file=args.relation_path + f'fold{fold_rmv[0]}_rmv{fold_rmv[1]}/' + dict2_name, mode='r'))
-        app_order_id2app_id = utils.process_dict_key(app_order_id2app_id)
-        app_id2app_order_id = utils.process_dict_key(app_id2app_order_id)
-
-        return relation, app_order_id2app_id, app_id2app_order_id               # 加载出来的键都是str类型，需要处理成int类型再使用
+        return relation
 
     size_apk: int = 0
     size_lib: int = 0
 
-    # 需要两个Dict, 分别从旧的映射到新的, 和新 -> 旧的
+    # 需要两个Dict, 映射appid和app的order id
     app_order_id2app_id: Dict = {}
     app_id2app_order_id: Dict = {}
+    # 同上
+    lib_order_id2lib_id: Dict = {}
+    lib_id2lib_order_id: Dict = {}
 
     train_fp = open(file=train_file_path, mode='r')
     for line in train_fp.readlines():
         obj = json.loads(line.strip('\n'))
         app_id = obj['app_id']
         tpl_list = obj['tpl_list']
-        # size_apk = max(size_apk, app_id)
         app_order_id2app_id[size_apk] = app_id
         app_id2app_order_id[app_id] = size_apk
         size_apk += 1
-        size_lib = max(max(tpl_list), size_lib)
+
+        for tpl in tpl_list:                                # 或许能够避免之前的nan错误....
+            if tpl not in lib_id2lib_order_id.keys():
+                lib_order_id2lib_id[size_lib] = tpl
+                lib_id2lib_order_id[tpl] = size_lib
+                size_lib += 1
 
     relation = np.zeros(shape=(size_apk, size_lib), dtype=np.int8)
 
@@ -52,18 +57,24 @@ def load_relation_mat(train_file_path: str) -> Tuple[np.ndarray, Dict, Dict]:
         tpl_list = obj['tpl_list']
         app_orderid = app_id2app_order_id[app_id]
         for tpl in tpl_list:
-            relation[app_orderid, tpl-1] = 1
+            lib_order_id = lib_id2lib_order_id[tpl]
+            relation[app_orderid, lib_order_id] = 1
 
     utils.ensure_dir(args.relation_path + f'fold{fold_rmv[0]}_rmv{fold_rmv[1]}/')
     np.savetxt(fname=args.relation_path + f'fold{fold_rmv[0]}_rmv{fold_rmv[1]}/' + relation_file_name, X=relation, fmt='%d')
-    # 两个字典也保存
-    fp1 = open(file=args.relation_path + f'fold{fold_rmv[0]}_rmv{fold_rmv[1]}/' + dict1_name, mode='w')
-    ws1 = json.dumps(app_order_id2app_id) + '\n'
-    fp1.write(ws1)
-    fp1.close()
-    fp2 = open(file=args.relation_path + f'fold{fold_rmv[0]}_rmv{fold_rmv[1]}/' + dict2_name, mode='w')
-    ws2 = json.dumps(app_id2app_order_id) + '\n'
-    fp2.write(ws2)
-    fp2.close()
+    # 保存app_id to app order id的映射关系，后面会用到
+    app_fp1 = open(file=args.relation_path + f'fold{fold_rmv[0]}_rmv{fold_rmv[1]}/' + dict1_name, mode='w')
+    ws1 = json.dumps(app_id2app_order_id) + '\n'
+    app_fp1.write(ws1)
+    app_fp1.close()
 
-    return relation, app_order_id2app_id, app_id2app_order_id
+    del ws1, app_fp1
+
+    lib_fp1 = open(file=args.relation_path + f'fold{fold_rmv[0]}_rmv{fold_rmv[1]}/' + lib_dict1_name, mode='w')
+    ws1 = json.dumps(lib_order_id2lib_id) + '\n'
+    lib_fp1.write(ws1)
+    lib_fp1.close()
+
+    del ws1, lib_fp1
+
+    return relation
